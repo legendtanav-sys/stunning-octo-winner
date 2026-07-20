9eda6aafaa1ead785ecbec330697478e33bb149a
legendtanav-sys <legend.tanav@gmail.com>
2026-07-20 18:38:48 +0530

    Add files via upload

--- 2.py
+++ 2.py
@@ -1,14 +1,18 @@
-#create class
-class Veichle:
+# create class
+class EMPLOYEE:
+    #initializinggggg
+    def __init__(self):
+        
+    # calling destructor
+            def __del__(self):
+                print("destructor called")
+def create_obj():
+     print('making object...')
+     obj = EMPLOYEE
+     print('function end')
+     return obj
 
-    #create init method
-    def __init__(self,max_speed,mileage):
-
-        #blind the arguments
-        self.max_speed = max_speed
-        self.mileage = mileage
-        #object creation
-modelx = Veichle(240, 18)
-        #access the variables init method
-print("model max speed", modelx.max_speed)
-print("model max speed", modelx.mileage)
+print('calling create_obj() function...')
+obj = create_obj
+print('program end.')
+del obj
